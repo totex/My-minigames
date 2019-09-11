02004302d409146c8d0027b0279aea48e497b1d0
totex <tonyfilpo@azet.sk>
2019-09-11 22:25:13 +0200

    removed Arcade-tiled-map-loading

--- Arcade-tiled-map-loading/my-map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="spritesheet_tiles" tilewidth="64" tileheight="64" spacing="10" tilecount="540" columns="27">
- <image source="spritesheet_tiles.png" width="1988" height="1470"/>
-</tileset>
